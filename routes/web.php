543b6113759a145e9d8260fc4429fa23a7e00756
Abraham Maleko <abrahammaleko@gmail.com>
2023-05-07 16:36:56 +0300

    removed duplicates already defined in auth routes

--- routes/web.php
+++ routes/web.php
@@ -30,28 +30,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth','verified'])->name('dashboard');
 
-//route for showing email verification notice
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
-
-
-// defines a route  that will handle requests generated when the user clicks
-// the email verification link that was emailed to them
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-
-//routes for resending email verification
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
 
 require __DIR__.'/auth.php';
 
