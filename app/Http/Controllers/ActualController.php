2b5f0d2dff7e98ac7bc72e4729d3f6387c5f3046
Abraham Maleko <abrahammaleko@gmail.com>
2023-05-13 18:57:19 +0300

    updated actual controller

--- app/Http/Controllers/ActualController.php
+++ app/Http/Controllers/ActualController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Actuals;
 use Illuminate\Http\Request;
 
 class ActualController extends Controller
@@ -11,7 +12,7 @@ class ActualController extends Controller
     {
         $this->middleware(['auth','verified','twofactor','change-password']);
     }
-    
+
     /**
      * Display a listing of the resource.
      *
@@ -33,27 +34,6 @@ class ActualController extends Controller
         return view('Actuals.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
@@ -63,29 +43,8 @@ class ActualController extends Controller
      */
     public function edit($id)
     {
-        //
+       $actual=Actuals::find($id);
+       return view('Actuals.edit',['actual' =>  $actual]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
