6a8fca9012509c9a4794cff4d361d0a49a63d1f8
Abraham Maleko <abrahammaleko@gmail.com>
2023-05-17 14:19:33 +0300

    drop unwanted columns

---
+++ database/migrations/2023_05_17_111630_drop_columns_in_assets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            //
+            $table->dropColumn(['gs_flag','cc_flag','ex_flag','uk_flag']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            //
+        });
+    }
+};
