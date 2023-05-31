706e1525b73b8cdf99d36fe52f050599f040a532
Abraham Maleko <abrahammaleko@gmail.com>
2023-05-31 21:46:33 +0300

    asset address should be unique in the table

--- app/Http/Livewire/Contracts/NewContract.php
+++ app/Http/Livewire/Contracts/NewContract.php
@@ -70,7 +70,8 @@ class NewContract extends Component
     public function addContractAsset(){
         $this->validate([
             'newAsset' => 'required',
-            'newAssetAddress' => 'required|string'
+            'newAssetAddress' => 'required|string|unique:contract_assets,asset_address'
+
         ]);
 
         //check for duplicates
