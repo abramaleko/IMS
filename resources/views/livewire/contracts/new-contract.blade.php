3bdeaf0fab246308a4603e18c409bd683d27b003
Abraham Maleko <abrahammaleko@gmail.com>
2023-06-14 12:43:51 +0300

    show project name on adding assets

--- resources/views/livewire/contracts/new-contract.blade.php
+++ resources/views/livewire/contracts/new-contract.blade.php
@@ -86,7 +86,7 @@
                                                             <select wire:model="newAsset" class=" form-control wide solid" >
                                                                 <option value="" selected disabled>Choose ..</option>
                                                                 @foreach ($assets as $asset)
-                                                                  <option value="{{$asset}}">{{$asset->asset_name}}</option>
+                                                                  <option value="{{$asset}}">{{$asset->asset_name}} - {{$asset->project->name}}</option>
                                                                 @endforeach
                                                             </select>
                                                             @error('newAsset')
